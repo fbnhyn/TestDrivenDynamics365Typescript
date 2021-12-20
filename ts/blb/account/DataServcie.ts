d7eeaa9fe57d6971523a8197502e51fd07c3d622
fbnhyn <fabian.heyne@outlook.com>
2021-12-20 11:32:36 +0100

    chore: format document

--- ts/blb/account/DataServcie.ts
+++ ts/blb/account/DataServcie.ts
@@ -1,15 +1,15 @@
 export namespace Blb.Account {
   export namespace DataService {
     export async function GetByName(name: string) {
-      return await XrmQuery.retrieveMultiple(x => x.accounts) 
-      .select(x => [ x.accountnumber ]) 
-      .filter(x => Filter.equals(x.name, name)) 
-      .promiseFirst()
+      return await XrmQuery.retrieveMultiple((x) => x.accounts)
+        .select((x) => [x.accountnumber])
+        .filter((x) => Filter.equals(x.name, name))
+        .promiseFirst();
     }
     export async function GetById(id: string) {
-      return await XrmQuery.retrieve(x => x.accounts, id) 
-      .select(x => [ x.accountnumber, x.name ]) 
-      .promise()
+      return await XrmQuery.retrieve((x) => x.accounts, id)
+        .select((x) => [x.accountnumber, x.name])
+        .promise();
     }
   }
 }
