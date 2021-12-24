8d567a6b42db10281ffe4a4e2788563f8eaf6d7c
fbnhyn <44057460+fbnhyn@users.noreply.github.com>
2021-12-24 13:04:14 +0100

    Delete ts/blb/account directory

--- ts/blb/account/_test/DataService.test.ts
+++
@@ -1,7 +0,0 @@
-import { Blb } from '../DataServcie';
-
-test('Mock DataService', async () => {
-  Blb.Account.DataService.GetByName = jest.fn().mockImplementationOnce(() => true);
-  const result = await Blb.Account.DataService.GetByName("Lufthansa");
-  return expect(result).toBe(true);
-});
